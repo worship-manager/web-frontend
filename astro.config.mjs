22faefde5e54557b62d80a7e6e0991ea98d33bf8
Tobani Esan-George <tobanigeorge@gmail.com>
2026-08-28 06:27:00 +0100

    Update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,15 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://worshipmanager.org",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-    react({ include: ["./src/**/*.tsx"] }),
-  ],
+  integrations: [mdx(), sitemap(), react(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,9 @@
-// 1. Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
-// 2. Define your collection(s)
 const blogCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: z.object({
     draft: z.boolean(),
     title: z.string(),
@@ -18,8 +19,6 @@ const blogCollection = defineCollection({
   }),
 });
 
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
 export const collections = {
   blog: blogCollection,
 };
